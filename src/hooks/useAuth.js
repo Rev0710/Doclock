5f3a4e8debfa45cb99f3e04bdba9be03515378ad
John Rev Dela Rosa <delarosa.johnrev071004@gmail.com>
2026-03-25 18:05:59 +0800

    Updated project

--- src/hooks/useAuth.js
+++
@@ -1,4 +0,0 @@
-// Convenience re-export so both import paths work:
-//   import { useAuth } from '../context/AuthContext'
-//   import useAuth from '../hooks/useAuth'
-export { useAuth as default } from '../context/AuthContext.jsx'
