fa8299c0b06a2e8019c3fef3b1229edf056d27d3
Ocomonsenpai <ryantacaisan13@gmail.com>
2026-03-28 02:05:53 +0800

    fssf

--- doclock-project/client/src/components/Footer.jsx
+++ doclock-project/client/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../assets/doc2.png'
+import logo from '../assets/whitelogo.png'
 import './Footer.css'
 
 function IconMail() {
